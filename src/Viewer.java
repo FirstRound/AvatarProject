2c7469561aaacc9318e648988683eea8e328052e
pisatel <vladpisatel@gmail.com>
2016-03-05 23:01:57 +0300

    Add spring window to Viewer. Add test images

--- src/Viewer.java
+++ src/Viewer.java
@@ -1,9 +1,13 @@
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Created by pisatel on 05.03.16.
  */
 public class Viewer implements Runnable{
 
     private volatile ConsumptionState _state;
+    private JFrame _frame;
 
     public Viewer() {
         _state = ConsumptionState.LOW;
@@ -14,8 +18,27 @@ public class Viewer implements Runnable{
     }
 
     public void run() {
+        _frame = new JFrame("Avatar");
+
+        ImageIcon image1 = new ImageIcon("./img/Luk.jpg");
+        JLabel imageLabel1 = new JLabel(image1);
+
+        ImageIcon image2 = new ImageIcon("./img/Vader.jpeg");
+        JLabel imageLabel2 = new JLabel(image2);
+
+        _frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        _frame.getContentPane().add(imageLabel2, BorderLayout.CENTER);
+        _frame.setSize(200, 300);
+        _frame.setVisible(true);
+
         while(true) {
             System.out.print(_state.toString() + "\n");
+            _frame.getContentPane().removeAll();
+            _frame.repaint();
+            if (_state == ConsumptionState.LOW)
+                _frame.getContentPane().add(imageLabel1, BorderLayout.CENTER);
+            else
+                _frame.getContentPane().add(imageLabel2, BorderLayout.CENTER);
             try {
                 Thread.sleep(1000);
             }
